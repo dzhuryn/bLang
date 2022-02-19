f61e8d61e9569325890b15636677cb599a0a43ab
Pathologic <m@xim.name>
2022-02-20 01:18:47 +0300

    yandex api update

--- assets/modules/blang/translator/yandex/index.php
+++ assets/modules/blang/translator/yandex/index.php
@@ -2,13 +2,9 @@
 
 use bLang\bLang;
 
-require_once 'src/Exception.php';
-require_once 'src/Translation.php';
-require_once 'src/Translator.php';
+require_once 'vendor/autoload.php';
 
-use Yandex\Translate\Translator;
-use Yandex\Translate\Exception;
-
+use Panda\Yandex\TranslateSdk;
 
 function yandexLangPrepare($lang,$langKeyResponse){
     $langKey = [];
@@ -28,19 +24,21 @@ function yandexLangPrepare($lang,$langKeyResponse){
 
 }
 
-function bLangTranslate($string,$from,$to,$bLang){
-    /** @var $bLang bLang */
-    $settings = $bLang->getSettings();
-
-
-    $from = yandexLangPrepare($from,$settings['yadexn_lang_key']);
-    $to = yandexLangPrepare($to,$settings['yadexn_lang_key']);
-
-    $translator = new Translator($settings['yandexKey']);
-    $translation = $translator->translate($string, $from.'-'.$to);
-
-
-    return (string) $translation;
-
+function bLangTranslate($string, $from, $to, $bLang)
+{
+    try {
+        $settings = $bLang->getSettings();
+	if (empty($settings['yandexKey'])) return;
+        $from = yandexLangPrepare($from,$settings['yadexn_lang_key']);
+        $to = yandexLangPrepare($to,$settings['yadexn_lang_key']);
+        $cloud = TranslateSdk\Cloud::createApi($settings['yandexKey']);
+        $translate = new TranslateSdk\Translate($string, $from);
+        $translate->setSourceLang($from)->setTargetLang($to);
+        $translate->setFormat(TranslateSdk\Format::HTML);
+        $result = json_decode($cloud->request($translate), true);
+        if (!empty($result['translations'][0]['text'])) return $result['translations'][0]['text'];
+    } catch (TranslateSdk\Exception\ClientException $e) {
+        
+    }
 
 }
---
+++ assets/modules/blang/translator/yandex/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'No version set (parsed as 1.0.0)',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'No version set (parsed as 1.0.0)',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'itpanda-llc/yandex-translate-sdk' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'e33afccdf70c54e22c19680ef364e9266d3d59ca',
+    ),
+  ),
+);
